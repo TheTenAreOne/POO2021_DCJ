40f7b5d2482b3fe855385ab62bf6d5f9dbbdd61b
TheTenAreOne <daniel.caicedoj@gmail.com>
2021-02-26 11:39:37 -0500

    add circulo.cpp

--- Tarea1-1.cpp
+++ Tarea1-1.cpp
@@ -3,11 +3,9 @@ using namespace std;
 const float IVA = 1.19;
 
 int main( ){
-    int precio;
+    float precio;
     cout << "Ingrese precio" << endl;
     cin >> precio;
     cout << "Precio final: " << precio * IVA << endl;
-
-
     return 0; 
 }

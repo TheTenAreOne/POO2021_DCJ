8fc8412c06465df65c6d9a071a5e86fa1c089ada
Thetenareone <daniel.caicedoj@gmail.com>
2021-05-12 16:49:52 -0500

    add juego masCercano

--- casino/Model/masCercano.cpp
+++ casino/Model/masCercano.cpp
@@ -2,12 +2,35 @@
 
 
 
-float masCercano( float gonzosApostar ){
-
-/*
-    El que se acerce más al número gana
-    
-*/
-
+float masCercano::jugar( float gonzosApostar ){
+    srand(time(NULL));
+    /*
+        El que se acerce más al número gana el doble
+        Si es empate, no se gana ni pierde
+        El numero objetivo es del 1 al 10
+    */
+    this->numero = 1+rand()%11; //Numero del 1 al 10
+    this->numeroCasino = 1+rand()%11; //Numero del 1 al 10
+    this->numeroJugador = 1+rand()%11; //Numero del 1 al 10
+    cout << "El numero objetivo es: "<< numero << "\n";
+    cout << "Tu numero aleatorio es: "<< numeroJugador <<"\n";
+    cout << "Numero casino: "<< numeroCasino <<"\n";
+    float gonzosResultado = calcularResultado( gonzosApostar );
+    return gonzosResultado;
 }
 
+float masCercano::calcularResultado( float gonzosApostar ){
+    //El jugador gana
+    if( abs( this->numero - this->numeroCasino) > abs( this->numero - this->numeroJugador) ){
+        return gonzosApostar*2;
+    }else if( abs( this->numero - this->numeroCasino) == abs(this->numero - this->numeroJugador)){
+        //Empate
+        return 0;
+    }
+    //El jugador pierde
+    return -gonzosApostar;
+}
+
+masCercano::~masCercano(){
+
+}
